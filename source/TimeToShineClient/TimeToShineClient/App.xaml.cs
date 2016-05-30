0080e5d9b5e06b3e0da00c059144697e695b998d
Jordan Knight <jakkaj@gmail.com>
2016-05-31 08:10:58 +1000

    Added default settings to the MQTT thing Added full screen mode

--- source/TimeToShineClient/TimeToShineClient/App.xaml.cs
+++ source/TimeToShineClient/TimeToShineClient/App.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -33,6 +34,7 @@ namespace TimeToShineClient
         {
             
             this.InitializeComponent();
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
             this.Suspending += OnSuspending;
         }
 
